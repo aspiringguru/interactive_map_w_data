8d1d5518d22f7815cb4687934aebdbd2652d967b
aspiringguru <bmatthewtaylor@gmail.com>
2020-04-14 08:18:02 +1000

    wip. cannot load new column of data.

--- README.txt
+++ README.txt
@@ -5,3 +5,7 @@
 > for enquiries email aspiringguruinoz@gmail.com
 
 > stay tuned as more demos in progress.
+
+
+ISO-3166 country codes from 
+https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes
---
+++ data/data_massage.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import unidecode
+#pip install Unidecode
+
+df_isocodes = pd.read_csv("ISO-3166_country_codes.csv")
+df_isocodes.shape
+df_isocodes.columns
+
+df_credit_ratings = pd.read_csv("world_countries_credit_ratings.csv")
+df_credit_ratings.shape
+df_credit_ratings.columns
+
+df_credit_ratings['country_'] = df_credit_ratings['Country'].str.lower().apply(unidecode.unidecode)
+df_isocodes['name_'] = df_isocodes['name'].str.lower().apply(unidecode.unidecode)
+
+set(df_isocodes['name_'])
+set(df_credit_ratings['country_'])
+
+set(df_isocodes['name_']) - set(df_credit_ratings['country_'])
+
+set(df_credit_ratings['country_']) - set(df_isocodes['name_'])
+
+
+#left join on df_credit_ratings['country_'] = df_isocodes['name_']
+#df1.merge(df2, left_on='lkey', right_on='rkey')
+
+df_cr_iso = pd.merge(df_credit_ratings,
+                 df_isocodes[['name_', 'alpha-3']],
+                 left_on='country_',
+                 right_on='name_',
+                 how='left')
+
+
+df_cr_iso.loc[df_cr_iso['alpha-3'].isna()].sort_values(by='Country', ascending=True)
+
+df_cr_iso.to_csv("credit_ratings_iso_codes.csv", index=False)
+df_cr_iso.shape
+df_cr_iso.columns
+#['Country', 'S&P', 'Moody's', 'Fitch', 'DBRS', 'TE', 'country_', 'name_', 'alpha-3']
+
+#now left merge df_cr_iso onto world_population.csv
+df_world_pop = pd.read_csv("world_population.csv")
+df_world_pop.shape
+df_world_pop.columns
+
+df_world_pop_cr_iso = pd.merge(df_world_pop,
+                 df_cr_iso[['name_', 'alpha-3']],
+                 left_on='country_',
+                 right_on='name_',
+                 how='left')
